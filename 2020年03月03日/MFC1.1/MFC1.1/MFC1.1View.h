703938e05d0aa4995313f53695f111301562ea3a
GreyballoonZeng <1243746650@qq.com>
2020-03-23 08:42:35 +0800

    2020/03/03

---
+++ 2020年03月03日/MFC1.1/MFC1.1/MFC1.1View.h
@@ -0,0 +1,46 @@
+
+// MFC1.1View.h : CMFC11View 类的接口
+//
+
+#pragma once
+
+
+class CMFC11View : public CView
+{
+protected: // 仅从序列化创建
+	CMFC11View();
+	DECLARE_DYNCREATE(CMFC11View)
+
+// 特性
+public:
+	CMFC11Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFC11View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFC1.1View.cpp 中的调试版本
+inline CMFC11Doc* CMFC11View::GetDocument() const
+   { return reinterpret_cast<CMFC11Doc*>(m_pDocument); }
+#endif
+
