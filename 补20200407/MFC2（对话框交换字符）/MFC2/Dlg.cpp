9ca40449fdb49e59d1289b2671033bd95a39f869
GreyballoonZeng <1243746650@qq.com>
2020-05-11 09:22:28 +0800

    2020/04/07

---
+++ 补20200407/MFC2（对话框交换字符）/MFC2/Dlg.cpp
@@ -0,0 +1,62 @@
+// Dlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "MFC2.h"
+#include "Dlg.h"
+#include "afxdialogex.h"
+
+
+// Dlg 对话框
+
+IMPLEMENT_DYNAMIC(Dlg, CDialogEx)
+
+Dlg::Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+	, s1(_T(""))
+	, s2(_T(""))
+{
+
+}
+
+Dlg::~Dlg()
+{
+}
+
+void Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, s1);
+	DDX_Text(pDX, IDC_EDIT2, s2);
+}
+
+
+BEGIN_MESSAGE_MAP(Dlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &Dlg::OnBnClickedButton1)
+	ON_EN_CHANGE(IDC_EDIT1, &Dlg::OnEnChangeEdit1)
+END_MESSAGE_MAP()
+
+
+// Dlg 消息处理程序
+
+
+void Dlg::OnBnClickedButton1()
+{
+	this->UpdateData(true);
+	CString s3;
+	s3 = s1;
+	s1 = s2;
+	s2 = s3;
+	this->UpdateData(false);
+}
+
+
+void Dlg::OnEnChangeEdit1()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
