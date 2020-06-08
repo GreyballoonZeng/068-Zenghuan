6f2b178a115af19bd7405a254332d9852e4e06a9
GreyballoonZeng <1243746650@qq.com>
2020-06-09 00:11:03 +0800

    2020/03/03

---
+++ 第一周/MFC1.3字符串拼接/MFC1.2/MFC1.2View.cpp
@@ -0,0 +1,95 @@
+
+// MFC1.2View.cpp : CMFC12View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC1.2.h"
+#endif
+
+#include "MFC1.2Doc.h"
+#include "MFC1.2View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC12View
+
+IMPLEMENT_DYNCREATE(CMFC12View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC12View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMFC12View 构造/析构
+
+CMFC12View::CMFC12View()
+{
+	// TODO: 在此处添加构造代码
+	B = _T("word!");
+
+}
+
+CMFC12View::~CMFC12View()
+{
+}
+
+BOOL CMFC12View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC12View 绘制
+
+void CMFC12View::OnDraw(CDC* pDC)
+{
+	CMFC12Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	CString s = _T("A=“Hello”, B=“word!”，点击左键进行拼接！");
+	pDC->TextOutW(10, 200, s);
+	
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFC12View 诊断
+
+#ifdef _DEBUG
+void CMFC12View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC12View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC12Doc* CMFC12View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC12Doc)));
+	return (CMFC12Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC12View 消息处理程序
+
+
+void CMFC12View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CMFC12Doc* pDoc = GetDocument();
+	CClientDC dc(this);
+	dc.TextOutW(point.x, point.y, pDoc->A + B);
+	CView::OnLButtonDown(nFlags, point);
+}
